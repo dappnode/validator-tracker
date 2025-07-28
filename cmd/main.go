401e88c568abd617549c78f3c025b028775ba91f
pablomendezroyo <mendez4a@gmail.com>
2025-07-28 10:09:11 +0200

    add todo comment

--- cmd/main.go
+++ cmd/main.go
@@ -18,6 +18,8 @@ import (
 	"github.com/dappnode/validator-tracker/internal/logger"
 )
 
+//TODO: Implement dev mode with commands example
+
 func main() {
 	// Load config
 	cfg := config.LoadConfig()
